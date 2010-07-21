2cf19931e2c4730023eb8c5c551aad08a4980637
Michael McGuire <mmcgu1966@gmail.com>
2010-07-20 22:09:42 -0700

    finished chapter 3 of RAILS

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,10 +1,45 @@
 class AdsController < ApplicationController
+	
+	before_filter :check_logged_in, :only => [:edit, :update, :destroy]
+	
+	def new
+		@ad = Ad.new
+	end
+	
+	def create
+		@ad=Ad.new(params[:ad])
+		@ad.save
+		redirect_to "/ads/#{@ad.id}"
+	end
+	
+	def edit
+		@ad = Ad.find(params[:id])		
+	end
+	
+	def update
+		@ad = Ad.find(params[:id])
+		@ad.update_attributes(params[:ad])
+		redirect_to "/ads/#{@ad.id}"
+	end
+	
 	def show
 		@ad = Ad.find(params[:id])
 	end
 	
 	def index
 		@ads = Ad.find(:all)
+	end	
+	
+	def destroy
+		@ad = Ad.find(params[:id])
+		@ad.destroy
+		redirect_to'/ads/'
 	end
 	
+	private
+		def check_logged_in
+			authenticate_or_request_with_http_basic("Ads") do |username, password|
+				username == "admin" && password == "password"
+			end	
+		end	
 end
